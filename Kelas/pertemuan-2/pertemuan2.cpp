6a7012f9bcbe410e2196ad5aac51606197851633
MamangolivE <zulfikararyawinata650@gmail.com>
2025-03-17 21:46:43 +0800

    Finish Post Test 2

---
+++ Kelas/pertemuan-2/pertemuan2.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+using namespace std;
+
+//string nama[4] = {"Haqi","Melchi","Dzaki"};
+// #define MAX_BUAH 100 // Ukuran maksimum array buah
+// int panjang = 0; // Jumlah elemen saat ini
+// string buah[MAX_BUAH]; // Array dengan ukuran tetap
+// int pilihan, index;
+
+//int main(){
+    //string arr[4] = {"Hello", "World", "1"};
+    //cout << arr[4];
+
+    //int angka[5] = {1, 2, 3, 4, 5}; //Menghitung ukuran tipedata
+    //cout << sizeof (angka); // terdapat 5 elemen, dan ukuran 1 tipedata int adalah 4, jadi hasilnya 5 * 4 = 20
+
+    //int panjangArray = sizeof(arr) / sizeof(arr(0))
+    //atau
+    //int panjangArray = sizeof(arr) / 
+    // do {
+    //     system("cls");
+    //     cout << "Menu Program" << endl;
+    //     cout << "1. Tampilkan Buah" << endl;
+    //     cout << "2. Tambah Buah" << endl;
+    //     cout << "3. Ubah Buah" << endl;
+    //     cout << "4. Hapus Buah" << endl;
+    //     cout << "5. Keluar" << endl;
+    //     cout << "Pilihan: ";
+    //     cin >> pilihan;
+    //     } 
+    //     while (pilihan != 5);
+    //         switch (pilihan) {
+    //             case 1:
+    //                 // Tampilkan buah
+    //                 break;
+    //             case 2:
+    //                 // Tambah buah
+    //                 break;
+    //             case 3:
+    //                 // Ubah buah
+    //                 break;
+    //             case 4:
+    //                 // Hapus buah
+    //                 break;
+    //             case 5:
+    //                 cout << "Program selesai" << endl;
+    //                 break;
+    //                 default:
+    //                     cout << "Pilihan tidak valid" << endl;
+    //                     break;
+    //         if (panjang < MAX_BUAH) {
+    //             cout << "Masukkan nama buah: ";
+    //             cin.ignore();
+    //             getline(cin, buah[panjang]);
+    //             panjang++;
+    //             cout << "Buah berhasil ditambahkan" << endl;
+    //             } 
+    //         else {
+    //             cout << "Kapasitas penuh! Tidak bisa menambah buah lagi."
+    //             << endl;
+    //             }
+    //         if (panjang == 0) {
+    //             cout << "Belum ada buah" << endl;
+    //             } 
+    //         else {
+    //             for (int i = 0; i < panjang; i++) {
+    //                 cout << "Buah ke-" << i + 1 << ": " << buah[i] << endl;
+    //                 }
+    //             }
+    //         if (panjang == 0) {
+    //             cout << "Belum ada buah untuk diubah." << endl;
+    //             } 
+    //         else {
+    //             for (int i = 0; i < panjang; i++) {
+    //                 cout << "Buah ke-" << i + 1 << ": " << buah[i] << endl;
+    //                 }
+    //                 cout << "Masukkan nomor buah yang akan diubah: ";
+    //                 cin >> index;
+    //                 if (index > 0 && index <= panjang) {
+    //                     cout << "Masukkan nama buah baru: ";
+    //                     cin.ignore();
+    //                     getline(cin, buah[index - 1]);
+    //                     cout << "Buah berhasil diubah" << endl;
+    //                     } 
+    //                 else {
+    //                     cout << "Nomor buah tidak valid" << endl;
+    //                     }
+    //                 }
+    //         if (panjang == 0) {
+    //             cout << "Belum ada buah untuk dihapus." << endl;
+    //             } 
+    //         else {
+    //             for (int i = 0; i < panjang; i++) {
+    //                 cout << "Buah ke-" << i + 1 << ": " << buah[i] << endl;
+    //                 }
+    //             cout << "Masukkan nomor buah yang akan dihapus: ";
+    //             cin >> index;
+    //             if (index > 0 && index <= panjang) {
+    //                 for (int i = index - 1; i < panjang - 1; i++) {
+    //                     buah[i] = buah[i + 1]; // Ngegeser elemen ke kiri kek nimpa jadinya
+    //                     }
+    //                     panjang--;
+    //                     cout << "Buah berhasil dihapus" << endl;
+    //                     } 
+    //             else {
+    //                 cout << "Nomor buah tidak valid" << endl;
+    //                 }
+    //             }
+//}
+
+// int main(){
+//     int matriks[2][2] = {
+//         {1, 2},
+//         {3, 4}
+//         };
+        
+//         cout << matriks[0][0] << endl;
+// }
+
+// int main(){
+//     int arr[3][3][3] = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}
+//         };
+        
+//         cout << arr[0][0][0] << endl;
+// }
---
+++ Kelas/pertemuan-2/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+#define MAX_BUAH 100 // Ukuran maksimum array buah
+// int panjang = 0; // Jumlah elemen saat ini
+// string buah[MAX_BUAH]; // Array dengan ukuran tetap
+// int pilihan, index;
